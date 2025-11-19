094e076fe0295ca72e0e13c3295dcdfa5ca26306
Jonsen Gaire <jonsen455@gmail.com>
2025-11-19 08:52:35 +0545

    Task 2

--- Workshop4/Program.cs
+++ Workshop4/Program.cs
@@ -1,30 +1,72 @@
-﻿class Student
-{
-    public string name;
-    public int age;
-    public string address;
-    public static string college = "ICP";
-}
+﻿/// <summary>
+/// Task 1
+/// </summary>
+// class Student
+// {
+//     public string name;
+//     public int age;
+//     public string address;
+//     public static string college = "ICP";
+// }
 
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Student s1 = new Student();
-        Student s2 = new Student();
-        s1.name = "Jonsen";
-        s1.age = 19;
-        s1.address = "Nayagaun";
-        s2.name = "Gaire";
-        s2.age = 20;
-        s2.address = "Rambazar";
-        Console.WriteLine("<---------------- STUDENT 1 ---------------->");
-        Console.WriteLine($"Name: {s1.name}, Age: {s1.age}, Address: {s1.address}");
-        Console.WriteLine("<---------------- STUDENT 2 ---------------->");
-        Console.WriteLine($"Name: {s2.name}, Age: {s2.age}, Address: {s2.address}");
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Student s1 = new Student();
+//         Student s2 = new Student();
+//         s1.name = "Jonsen";
+//         s1.age = 19;
+//         s1.address = "Nayagaun";
+//         s2.name = "Gaire";
+//         s2.age = 20;
+//         s2.address = "Rambazar";
+//         Console.WriteLine("<---------------- STUDENT 1 ---------------->");
+//         Console.WriteLine($"Name: {s1.name}, Age: {s1.age}, Address: {s1.address}");
+//         Console.WriteLine("<---------------- STUDENT 2 ---------------->");
+//         Console.WriteLine($"Name: {s2.name}, Age: {s2.age}, Address: {s2.address}");
 
-        Console.WriteLine($"College: {Student.college}");
-    }
-}
+//         Console.WriteLine($"College: {Student.college}");
+//     }
+// }
+
+
+/// <summary>
+/// Task 2
+/// </summary>
+// class Calculator
+// {
+//     public void PrintWelcome()
+//     {
+//         Console.WriteLine("Welcome to the calculator!");
+//     }
+
+//     public int Add(int a, int b)
+//     {
+//         return a + b;
+//     }
+//     publicint Multiply(int a, int b=1)
+//     {
+//         return a * b;
+//     }
+// }
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Calculator calculator = new Calculator();
+//         calculator.PrintWelcome();
+//         Console.WriteLine($"Addition: {calculator.Add(2, 3)}");
+//         Console.WriteLine($"Multiplication with default value: {calculator.Multiply(2)}");
+//         Console.WriteLine($"Multiplication with two numbers: {calculator.Multiply(2, 3)}");
+//     }
+// }
+
+
+///<summary>
+/// Task 3
+/// </summary>
+
 
