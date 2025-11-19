96197334c99f1d312eeabedd8c9465bbfd54484d
Jonsen Gaire <jonsen455@gmail.com>
2025-11-19 08:52:58 +0545

    Task 2.1

--- Workshop4/Program.cs
+++ Workshop4/Program.cs
@@ -35,38 +35,35 @@
 /// <summary>
 /// Task 2
 /// </summary>
-// class Calculator
-// {
-//     public void PrintWelcome()
-//     {
-//         Console.WriteLine("Welcome to the calculator!");
-//     }
+class Calculator
+{
+    public void PrintWelcome()
+    {
+        Console.WriteLine("Welcome to the calculator!");
+    }
 
-//     public int Add(int a, int b)
-//     {
-//         return a + b;
-//     }
-//     publicint Multiply(int a, int b=1)
-//     {
-//         return a * b;
-//     }
-// }
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    publicint Multiply(int a, int b=1)
+    {
+        return a * b;
+    }
+}
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Calculator calculator = new Calculator();
-//         calculator.PrintWelcome();
-//         Console.WriteLine($"Addition: {calculator.Add(2, 3)}");
-//         Console.WriteLine($"Multiplication with default value: {calculator.Multiply(2)}");
-//         Console.WriteLine($"Multiplication with two numbers: {calculator.Multiply(2, 3)}");
-//     }
-// }
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        calculator.PrintWelcome();
+        Console.WriteLine($"Addition: {calculator.Add(2, 3)}");
+        Console.WriteLine($"Multiplication with default value: {calculator.Multiply(2)}");
+        Console.WriteLine($"Multiplication with two numbers: {calculator.Multiply(2, 3)}");
+    }
+}
 
 
-///<summary>
-/// Task 3
-/// </summary>
 
 
